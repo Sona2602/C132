62574e263814df58bcac2132fdcab2dab21e5f00
Sonakshi <96131236+Sona2602@users.noreply.github.com>
2023-04-27 20:41:45 +0530

    Add files via upload

---
+++ fruits.js
@@ -0,0 +1,34 @@
+img = "";
+Status = "";
+function home()
+{
+    window.location = "index.html";
+}
+function setup()
+{
+    canvas = createCanvas(550,330);
+    objectDetection = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function draw()
+{
+    image(img,0,0,550,330);
+}
+function preload()
+{
+    img = loadImage("delicious-fruit-basket.jpg");
+}
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    Status = true;
+    objectDetection.detect(img,gotResult);
+}
+function gotResult(error,results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+}
